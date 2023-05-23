9ecc1e9e491e856a6c9a131204c74ef95c4530cc
Lloyd <lloyddraysey@gmail.com>
2023-05-23 21:21:03 +0930

    Added commenting to all files

--- PokedexTest.cpp
+++ PokedexTest.cpp
@@ -12,41 +12,49 @@ extern vector<vector<string>> pokemonDatabase();
 
 int main(){
 
+    // Load the Pokemon database
     vector<vector<string>> database = pokemonDatabase();
 
+    // Boolean flags to control drawing of menu and Pokedex
     bool drawMenu = false;
     bool drawPokedex = false;
 
     int pagePokedex = 1;
 
+    // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 600), "Menu Test");
 
-    Menu menu(0,0);
-
+    // Create an instance of Menu
+    Menu menu(0, 0);
+    //Set clock for checking time since opened
     sf::Clock clock;
 
+    // Create an instance of Pokedex
     Pokedex Pokedex(database, 0, 0);
-
 
     while (window.isOpen()){
 
+        // Check if 'M' key is pressed to toggle the menu
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::M)){
             if ((int)((clock.getElapsedTime()).asSeconds()) > 1){
-                drawMenu ^= true;
-                clock.restart();
+                drawMenu ^= true; // Toggle the drawMenu flag
+                clock.restart(); //Reset clock
             }
         }
 
+        // Check if Escape key is pressed to hide the menu and Pokedex
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
             drawMenu = false;
+            drawPokedex = false;
         }
 
-
+        // Check if Enter key is pressed and the menu is drawn
         if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Enter) && drawMenu) && (menu.getItemIndex() == 0)){
-            drawPokedex ^= true;
-            drawMenu ^= true;
+            drawPokedex ^= true; // Toggle the drawPokedex flag
+            drawMenu ^= true; // Toggle the drawMenu flag
         }
 
+        // Handle window events
         sf::Event event;
         while (window.pollEvent(event)){
             switch(event.type){
@@ -54,25 +62,25 @@ int main(){
                     switch (event.key.code){
                         case sf::Keyboard::Up:
                             if (drawMenu == true){
-                                menu.moveUp();
+                                menu.moveUp(); // Move the selection up in the menu
                             }
                             if((pagePokedex -1 > 0) && drawPokedex){
-                                cout << Pokedex.getItemIndex();
-                                Pokedex.PositionChange();
-                                pagePokedex--;
-                                cout << Pokedex.getItemIndex();
+                                cout << Pokedex.getItemIndex();//Testing before
+                                Pokedex.PositionChange(); // Adjust the position of the items in the Pokedex
+                                pagePokedex--; //Change page
+                                cout << Pokedex.getItemIndex(); //Testing change
 
                             }
                             break;
                         case sf::Keyboard::Down:
                             if (drawMenu == true){
-                                menu.moveDown();
+                                menu.moveDown(); // Move the selection down in the menu
                             }
                             if ((pagePokedex + 1 < (151)) && drawPokedex){
-                                cout << Pokedex.getItemIndex();
-                                Pokedex.PositionChange();
-                                pagePokedex++;
-                                cout << Pokedex.getItemIndex();
+                                cout << Pokedex.getItemIndex(); //Testing before
+                                Pokedex.PositionChange(); // Adjust the positions of items in the Pokedex
+                                pagePokedex++; //Change page
+                                cout << Pokedex.getItemIndex(); //Testing after changes
 
                             }
                             break;
@@ -90,20 +98,25 @@ int main(){
 
         window.clear(sf::Color::White);
 
+        // Draw the menu if the flag is set
         if (drawMenu){
             menu.setPosition(0, 450);
             menu.draw(window);
         }
 
+        // Draw the Pokedex if the flag is set
         if (drawPokedex){
+            //Output the current position to test the change
             cout << "Current Pos = "<< Pokedex.getPosition()[0];
             cout << "Current Pos" <<Pokedex.getPosition()[1];
             Pokedex.setPosition(0,450);
             cout << "Current Pos = "<< Pokedex.getPosition()[0];
             cout << "Current Pos = " <<Pokedex.getPosition()[1];
+            //Draw the pokedex dependant on the page
             Pokedex.draw(window, pagePokedex);
 
         }
+        //Display the window
         window.display();
     }
     return 0;
